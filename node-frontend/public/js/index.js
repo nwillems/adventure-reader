d2489156523463f1022f22509fb6d316d0adeb56
Nicolai Willems <niwil90@gmail.com>
2013-09-03 19:37:26 +0200

    Many updates and functionality

--- node-frontend/public/js/index.js
+++ node-frontend/public/js/index.js
@@ -19,23 +19,53 @@ $(document).ready(function(){
 
 var App = window.App;
 
+App.getUserID = function(){
+    if(App.profile){
+        return App.profile.id;
+    }
+    return 0;
+}
+
 App.Initialize = function(){
 
     App.Model = new AdventureReader.AdventureReaderViewModel();
 
     ko.applyBindings(App.Model);
 
-    $.ajax('/api/feeds', { success: function(res){
-        var feeds = res.feeds;
+    $.ajax({ 
+        url: '/api/feeds', 
+        success: function(res){
+            var feeds = res.feeds;
 
-        App.Model.feeds.removeAll();
+            App.Model.feeds.removeAll();
 
-        for(var i = 0; i < feeds.length; i++){
-            var feed = feeds[i];
-            App.Model.feeds.push(CreateFeedModel(feed));
+            for(var i = 0; i < feeds.length; i++){
+                var feed = feeds[i];
+                App.Model.feeds.push(CreateFeedModel(feed));
+            }
+            console.log(res);
         }
-        console.log(res);
-    }});
+    });
 
-    console.log("Initializing");
+    $.ajax({
+        url: '/api/users',
+        type: 'POST',
+        data: { 'username' : App.profile.displayName , 'userid': App.getUserID() },
+        success: function(result){
+            console.log(result);
+        }
+    });
+
+   $('#addFeed').on('click', null, null, function(e){
+        //Show input field
+        $.ajax({
+            url : '/api/users/'+App.getUserID()+'/feeds',
+            type: 'PUT',
+            data: { url: $('#feedToAdd')[0].value },
+            success: function(){
+                console.log("Added feed", arguments);
+            }
+        });
+   });
+
 };
--- node-frontend/public/js/signin.js
+++ node-frontend/public/js/signin.js
@@ -1,18 +1,49 @@
 if(!window.App) window.App = {};
 
-function signout(){
-    $.ajax({
-        type: 'GET',
-        url: 'https://accounts.google.com/o/oauth2/revoke',
-        data: { token: App.authentication['access_token'] },
-        async: false,
-        dataType: 'jsonp',
-        success: function(rsp){
-            console.log("Succesfully signed out");
-        },
-        error: function(e){ console.log(e); }
-    });
-}
+window.App.Signin = (function(){ return {
+    signout: function signout(){
+        $.ajax({
+            type: 'GET',
+            url: 'https://accounts.google.com/o/oauth2/revoke',
+            data: { token: App.authentication['access_token'] },
+            async: false,
+            dataType: 'jsonp',
+            success: function(rsp){
+                console.log("Succesfully signed out");
+            },
+            error: function(e){ console.log(e); }
+        });
+    },
+    onSignin : function onSignin(auth){
+        gapi.client.load('plus','v1', function(){
+            if(auth['access_token']){
+                window.App.authentication = auth;
+                $('#loginModal').modal('hide');
+                $('#logout').on('click', App.Signin.signout);
+
+                var request = gapi.client.plus.people.get( {'userId' : 'me'} );
+                request.execute( function(profile) {
+                    window.App.profile = profile;
+                    window.App.Initialize();
+                });
+            }else if(auth['error']){
+                // show modal
+                $('#loginModal').modal({ show: true, keyboard: false });
+                console.log("Error during signin", auth);
+            }
+        });
+    },
+    renderSignin : function renderSignin(){
+        gapi.signin.render('signinBtn', {
+            callback: App.Signin.onSignin,
+            clientid: "166612591389.apps.googleusercontent.com",
+            cookiepolicy: "single_host_origin",
+            requestvisibleactions: "http://schemas.google.com/AddActivity",
+            scope: "https://www.googleapis.com/auth/plus.login"
+        });
+    }
+}}());
+
 
 (function() {
     var po = document.createElement('script'); po.type = 'text/javascript';
@@ -22,25 +53,6 @@ function signout(){
     s.parentNode.insertBefore(po, s);
 })();
 
-function signinCallback(auth){
-    if(auth['error']){
-        // show modal
-        $('#loginModal').modal({ show: true, keyboard: false });
-    }else{
-        window.App.authentication = auth;
-        $('#loginModal').modal('hide');
-        $('#logout').on('click', signout);
-
-        window.App.Initialize();
-    }
-}
-
 function renderSignin(){
-    gapi.signin.render('signinBtn', {
-        callback: "signinCallback",
-        clientid: "166612591389.apps.googleusercontent.com",
-        cookiepolicy: "single_host_origin",
-        requestvisibleactions: "http://schemas.google.com/AddActivity",
-        scope: "https://www.googleapis.com/auth/plus.login"
-    });
+    App.Signin.renderSignin();
 }
